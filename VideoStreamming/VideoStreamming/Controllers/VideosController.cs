ccbb2cd322eff96b95975e008d3b139cf8ed0b4d
myatthu1986 <myatthu1986@gmail.com>
2016-02-29 17:34:26 +0800

    video streamming without indexable

---
+++ VideoStreamming/VideoStreamming/Controllers/VideosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using VideoStreamming.Models.BusinessLogic;
+
+namespace VideoStreamming.Controllers
+{
+    public class VideosController : ApiController
+    {
+        public HttpResponseMessage Get(string filename, string ext)
+        {
+            var video = new VideoStream(filename, ext);
+
+            var response = Request.CreateResponse();
+            response.Content = new PushStreamContent((Action<Stream, HttpContent, TransportContext>)video.WriteToStream);
+            //response.Content = new PushStreamContent(video.WriteToStream, new MediaTypeHeaderValue("video/" + ext));
+            return response;
+        }
+    }
+}
---
+++ VideoStreamming/VideoStreamming/Models/BusinessLogic/VideoStream.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace VideoStreamming.Models.BusinessLogic
+{
+    public class VideoStream
+    {
+        private readonly string _filename;
+
+        public VideoStream(string filename, string ext)
+        {
+            _filename = string.Format("D:\\Test\\{0}.{1}", filename, ext);
+        }
+
+        public async void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        {
+            try
+            {
+                using (var video = File.Open(_filename, FileMode.Open, FileAccess.Read))
+                {
+                    var length = (int)video.Length;
+                    var buffer = new byte[length];
+                    var bytesRead = 1;
+
+                    while (length > 0 && bytesRead > 0)
+                    {
+                        bytesRead = video.Read(buffer, 0, Math.Min(length, buffer.Length));
+                        await outputStream.WriteAsync(buffer, 0, bytesRead);
+                        length -= bytesRead;
+                    }
+                }
+            }
+            catch (HttpException ex)
+            {
+                return;
+            }
+            finally
+            {
+                outputStream.Close();
+            }
+        }
+    }
+}
